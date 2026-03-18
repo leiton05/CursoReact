824a54e9f9762b1d3d719585557197e449aff0af
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 20:23:01 -0500

    ✨Feat: Add Item Counter logic

--- React/02-first-steps/src/FirstStepsApp.tsx
+++ React/02-first-steps/src/FirstStepsApp.tsx
@@ -1,10 +1,28 @@
-import ItemCounter from "./shopping-cart/itemCounter";
+import { ItemCounter } from "./shopping-cart/ItemCounter";
+
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+const itemsInCart: ItemInCart[] = [
+  { productName: "Arthur", quantity: 1 },
+  { productName: "Squirtle", quantity: 450 },
+  { productName: "Latias", quantity: 14 },
+  { productName: "Bidoof", quantity: 1 },
+];
 
 export function FirstStepsApp() {
   return (
     <>
       <h1>Carrito de compras</h1>
-      <ItemCounter />
+      {itemsInCart.map((item) => (
+        <ItemCounter
+          key={item.productName}
+          name={item.productName}
+          quantity={item.quantity}
+        />
+      ))}
     </>
   );
 }
--- React/02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ React/02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+interface ItemProps {
+  name: string;
+  quantity: number;
+}
 
-const ItemCounter = () => {
-  return <div>ItemCounter</div>;
+export const ItemCounter = ({
+  name: actualName,
+  quantity: amount,
+}: ItemProps) => {
+  console.log(actualName);
+  return (
+    <section
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: 10,
+        marginTop: 10,
+      }}
+    >
+      <span
+        style={{
+          width: 150,
+        }}
+      >
+        {actualName}
+      </span>
+      <button
+        onClick={(event) => {
+          console.log(`"Click en " + ${actualName}`);
+        }}
+      >
+        -1
+      </button>
+      <span>{amount}</span>
+      <button>+1</button>
+    </section>
+  );
 };
-
-export default ItemCounter;
